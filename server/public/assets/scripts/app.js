0e71dc5680fd18759f4abc4f853f3877946c451e
bpeterson1663 <bpeterson1663@gmail.com>
2016-03-23 15:22:29 -0500

    Update code

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,51 +1,48 @@
 var myApp = angular.module("myApp",[]);
+var studentArray;
 
 myApp.controller("MainController", ["$scope","$http", function($scope, $http){
   //refresh button get student data, shuffle, assign teams and display
   $scope.groupArray = [];
+
   $scope.assignNumGroups = function(number){
     $scope.numGroups = number;
-
+    console.log("Group number : ", $scope.numGroups);
   };
 
   $scope.getStudentData = function(){
+
+
     $http.get("/kappaStudents").then(function(response){
       $scope.studentArray = response.data.students;
-      return $scope.studentArray;
+      //console.log("Inside Get Request: ", $scope.studentArray);
     });
+    //return $scope.studentArray;
+  };
+
+
 
   $scope.refresh = function(){
 
-      var allStudents = $scope.getStudentData();
-      allStudents = shuffleArray(allStudents);
+      // var allStudents = $scope.studentArray;
+      $scope.studentArray = shuffleArray($scope.studentArray);
+      // console.log('All Students array', allStudents);
 
-
-      for(var i = allStudents.length -1; i >0; i--){
+      for(var i = $scope.studentArray.length - 1; i >0; i--){
           var targetNum = i % $scope.numGroups;
-          var student = studentArray.pop();
+          var student = $scope.studentArray.pop();
+          console.log("Student was popped: ", student)
           $scope.groupArray[targetNum].push(student);
         }
         console.log("Group Array: ", $scope.groupArray);
   };
 
-
-  $scope.getStudentData = function(){
-    $http.get("/kappaStudents").then(function(response){
-      $scope.studentArray = response.data.students;
-      console.log($scope.studentArray);
-    });
-
-    console.log("Function being executed");
-  };
-
+$scope.getStudentData();
 }]);//end of MainController
 
 
-
-
-
-
 function shuffleArray(array) {
+    //console.log("Array", array);
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = array[i];
