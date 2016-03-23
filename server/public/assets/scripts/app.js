d6c3a60bde2951cf08595c2cadf3b8c34e9fd515
bpeterson1663 <bpeterson1663@gmail.com>
2016-03-23 13:55:23 -0500

    Collected data from json file

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,6 +1,7 @@
 var myApp = angular.module("myApp",[]);
 
 myApp.controller("MainController", ["$scope","$http", function($scope, $http){
+  //refresh button get student data, shuffle, assign teams and display
 
   $scope.getStudentData = function(){
     $http.get("/kappaStudents").then(function(response){
@@ -12,5 +13,14 @@ myApp.controller("MainController", ["$scope","$http", function($scope, $http){
   };
 
   $scope.getStudentData();
+}]);//end of MainController
 
-}]);
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
